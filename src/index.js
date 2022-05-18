a9429f57494573d9aef76c80a639f70b13d30548
Eduarda Calderan <eduardanicolodicalderan@gmail.com>
2022-05-18 18:41:45 -0300

    cadastro de conta

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello World -  Fundamentos Node.js" });
+const customers = [];
+
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
+  const id = uuidv4();
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: [],
+  });
+
+  return res.status(201).send();
 });
 
 app.listen(3333);
