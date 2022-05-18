027b9a22bf4e6cb1daa79abd2b47a9ee94962864
Eduarda Calderan <eduardanicolodicalderan@gmail.com>
2022-05-18 18:48:08 -0300

    validando cpf existente

--- src/index.js
+++ src/index.js
@@ -8,12 +8,19 @@ const customers = [];
 
 app.post("/account", (req, res) => {
   const { cpf, name } = req.body;
-  const id = uuidv4();
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return res.status(400).json({ errpr: "Customer already exists" });
+  }
 
   customers.push({
     cpf,
     name,
-    id,
+    id: uuidv4(),
     statement: [],
   });
 
