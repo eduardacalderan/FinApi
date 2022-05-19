268093184097756b5153c6528def7273f4b05993
Eduarda Calderan <eduardanicolodicalderan@gmail.com>
2022-05-19 17:13:51 -0300

    criação de middleware

--- src/index.js
+++ src/index.js
@@ -6,6 +6,21 @@ app.use(express.json());
 
 const customers = [];
 
+//MIDDLEWARE
+function verifyIfExistsAccountCPF(req, res, next) {
+  const { cpf } = req.params;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return res.status(400).json({ error: "Customer not found" });
+  }
+
+  req.customer = customer;
+
+  return next();
+}
+
 app.post("/account", (req, res) => {
   const { cpf, name } = req.body;
 
@@ -27,11 +42,8 @@ app.post("/account", (req, res) => {
   return res.status(201).send();
 });
 
-app.get("/statement/:cpf", (req, res) => {
-  const { cpf } = request.params;
-
-  const customer = constumers.find((customer) => customer.cpf === cpf);
-
+app.get("/statement", verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
   return res.json(customer.statement);
 });
 
